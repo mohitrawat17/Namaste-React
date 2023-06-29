49560ed10a683fb05e7d74b001941a8421fbf875
Mohit <mr6114671@gmail.com>
2023-06-29 09:28:22 +0530

    igniting app

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement('h1', {style:{color:"red"}, id:"heading",key:"1"}, 'Hello React !');
+const head1 = React.createElement('h2', {key:"2"}, 'heading 1');
+const head2 = React.createElement('h2', {key:"3"}, 'heading 2');
+
+const cont=React.createElement('div',{id:"container"},[heading,head1,head2])
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(cont)
