f295d9fd65af4a6a721a85370cb4952f8eefb23e
Mohit <mr6114671@gmail.com>
2023-06-29 12:09:08 +0530

    igniting app part 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement('h1', {style:{color:"red"}, id:"heading",key:"1"}, 'Hello React !');
 const head1 = React.createElement('h2', {key:"2"}, 'heading 1');
 const head2 = React.createElement('h2', {key:"3"}, 'heading 2');
